fba913554640f888faabb7c437b2a130fb8996a0
Mugundhan Gc <mugundhangc@rubikssyslabs.com>
2024-01-10 16:42:40 +0530

    Initial Commit

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,16 @@
 import express from 'express';
 import connectToDB from '../src/database/db';
 import AppConstants from '../src/utls/constants';
+import bodyParser from 'body-parser';
 const logger = require('./helpers/logger');
 const routes = require('./routes/route')
 const appConstant = new AppConstants();
 
 const app = express();
 const port = 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 connectToDB()
   .then((db:any) => {
