0eb35c3132a838be976e4b3cd422a588ae6b3e2c
Mugundhan Gc <mugundhangc@rubikssyslabs.com>
2024-03-07 13:35:26 +0530

    Esign Changes done

--- src/services/esign.ts
+++ src/services/esign.ts
@@ -23,32 +23,33 @@ export class eSignService {
 
             const envelope_api = await eSign.getEnvelopesApi(token_data.access_token)
 
-            // const filepath = path.join(__dirname, "Payer_7a578154-8922-4f17-9614-40e901bcc260.pdf")
-            const filepath1 = path.join(__dirname, "Payer_7a578154-8922-4f17-9614-40e901bcc260.pdf")
-            const filepath2 = path.join(__dirname, "Payer_7a578154-8922-4f17-9614-40e901bcc260.pdf")
+            const filepath = path.join(__dirname, "Payer_7a578154-8922-4f17-9614-40e901bcc260.pdf")
+            // const filepath1 = path.join(__dirname, "Payer_7a578154-8922-4f17-9614-40e901bcc260.pdf")
+            // const filepath2 = path.join(__dirname, "Payer_7a578154-8922-4f17-9614-40e901bcc260.pdf")
             // const filepath3 = path.join(__dirname, "1709052355542-Degree.pdf")
 
-            // const envelope = await eSign.makeEnvelope(filepath, body_data.email, body_data.name)
-            const envelope = await eSign.makeEnvelopeWithMultipleDoc(filepath1, filepath2, body_data.email, body_data.name)
+            const envelope = await eSign.makeEnvelope(filepath, body_data.email, body_data.name)
+            // const envelope = await eSign.makeEnvelopeWithMultipleDoc(filepath1, filepath2, body_data.email, body_data.name)
 
 
-        const create_envople = await envelope_api.createEnvelope(
-            process.env.ACCOUNT_ID, { envelopeDefinition: envelope }
-        )
+            const create_envople = await envelope_api.createEnvelope(
+                process.env.ACCOUNT_ID, { envelopeDefinition: envelope }
+            )
 
             console.log("🚀 ~ eSignService ~ getEsignURI ~ create_envople.envelopeId:", create_envople.envelopeId)
 
-        const viewRequest = await eSign.getDocusignRedirectUrl(body_data.email, body_data.name)
+            const viewRequest = await eSign.getDocusignRedirectUrl(body_data.email, body_data.name)
 
-        const final_uri = await envelope_api.createRecipientView(
-            process.env.ACCOUNT_ID,
-            create_envople.envelopeId,
-            { recipientViewRequest: viewRequest }
-        )
+            const final_uri = await envelope_api.createRecipientView(
+                process.env.ACCOUNT_ID,
+                create_envople.envelopeId,
+                { recipientViewRequest: viewRequest }
+            )
 
-        return { message: 'Successfully retrive Redirect URL', data: final_uri.url, envelope_id: create_envople.envelopeId }
-    } catch(error: any) {
-        console.log(error)
+            return { message: 'Successfully retrive Redirect URL', data: final_uri.url, envelope_id: create_envople.envelopeId }
+        } catch (error: any) {
+            console.log(error)
+        }
     }
 
     async getEsignList(user_data: { id: string, user_type: string }, filter_data?: any) {
