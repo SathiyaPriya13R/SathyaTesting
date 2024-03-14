a1e669c06fb4adbb6f1943d3dd03cce300aa7b63
selvakannan.r <selvakannan.r@rubikssyslabs.com>
2024-03-14 22:33:04 +0530

    #notificatio and alert for document changes

--- src/helpers/cronjobs.ts
+++ src/helpers/cronjobs.ts
@@ -7,6 +7,7 @@ const cron = require('node-cron');
 const userService = new UserService();
 
 export default class Corn {
+
     async generatePassword() {
         const task = cron.schedule('0 0 */1 * * *', () => {
             userService.generatePasswordCron().then(() => {
@@ -16,4 +17,25 @@ export default class Corn {
             });
         });
     }
+
+    // async pushNotification() {
+    //     const task = cron.schedule('0 0 */1 * * *', () => {
+    //         userService.generatePasswordCron().then(() => {
+    //             logger.info(__filename, appConstant.CRON_MESSAGE.GENERATE_PASSWORD_FUNCTION_COMPLETED);
+    //         }).catch((error: any) => {
+    //             logger.info(__filename, appConstant.CRON_MESSAGE.GENERATE_PASSWORD_FUNCTION_FAILED, error);
+    //         });
+    //     });
+    // }
+
+    // async pushAlert() {
+    //     const task = cron.schedule('0 0 */1 * * *', () => {
+    //         userService.generatePasswordCron().then(() => {
+    //             logger.info(__filename, appConstant.CRON_MESSAGE.GENERATE_PASSWORD_FUNCTION_COMPLETED);
+    //         }).catch((error: any) => {
+    //             logger.info(__filename, appConstant.CRON_MESSAGE.GENERATE_PASSWORD_FUNCTION_FAILED, error);
+    //         });
+    //     });
+    // }
+
 }
