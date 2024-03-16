1aebe283cbb63a40a4e3ae6450666838f6459d49
Mugundhan Gc <mugundhangc@rubikssyslabs.com>
2024-03-16 22:10:33 +0530

    User changes

--- src/middleware/authguard.ts
+++ src/middleware/authguard.ts
@@ -58,6 +58,7 @@ app.use(async function (req: Request, res: Response, next) {
                             validToken = data;
                         }
                     })
+                    console.log('validToken ---',validToken);
                     if (tokenValid && !_.isEmpty(tokenValid) && validToken && validToken.authToken == token) {
                         // Check expiration time
                         const currentTimestamp = Math.floor(Date.now() / 1000);
